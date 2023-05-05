bcc05cf61894871d18663e5668863a362fa519b8
ArfazAhamed <arfazahamed31@gmail.com>
2023-05-05 18:22:30 +0600

    allChefData get

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const app = express()
 const port = 5000
 
 
+const allChefData = require('./data.json')
 
 app.get('/', (req, res) => {
     res.send('Hello Chef')
   })
 
 
-
+  app.get('/chef', (req, res)=>{
+    res.send(allChefData)
+})
 
 app.listen(port, () => {
     console.log(`FoodHut server is running ${port}`)

9aa52db47432258a031b3b8d24789908293ea12d
ArfazAhamed <arfazahamed31@gmail.com>
2023-05-05 21:41:08 +0600

    single chef data find with id

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000
 
+
+app.use(cors())
 
 const allChefData = require('./data.json')
 
@@ -13,6 +16,13 @@ app.get('/', (req, res) => {
   app.get('/chef', (req, res)=>{
     res.send(allChefData)
 })
+
+app.get('/chef/:id', (req, res)=>{
+    const id = req.params.id;
+    const chefsData = allChefData.find(chef=> chef.id == id) || {};
+    res.send(chefsData)
+})
+
 
 app.listen(port, () => {
     console.log(`FoodHut server is running ${port}`)
